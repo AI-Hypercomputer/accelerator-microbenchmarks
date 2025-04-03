83dbd33a3b2f9639793f5c9344a95165a51e7152
Raymond Zou <raymondzou@google.com>
2025-04-03 00:43:20 +0000

    Increase tries to 1000

--- src/benchmark_utils.py
+++ src/benchmark_utils.py
@@ -7,21 +7,20 @@ import jax
 import jsonlines
 
 
-def simple_timeit(f, *args, tries=10, task=None):
+def simple_timeit(f, *args, tries=1000, task=None):
   """Simple utility to time a function for multiple runs."""
   assert task is not None
 
-  outcomes_ms = []
   jax.block_until_ready(f(*args))  # warm it up!
+  s = datetime.datetime.now()
   for _ in range(tries):
     jax.devices()  # Force synchronization across devices
-    s = datetime.datetime.now()
-    jax.block_until_ready(f(*args))
-    e = datetime.datetime.now()
-    outcomes_ms.append(1000 * (e - s).total_seconds())
+    data = f(*args)
+  jax.block_until_ready(data)
+  e = datetime.datetime.now()
+  average_time_ms = (1000 * (e - s).total_seconds())/tries
 
-  average_time_ms = sum(outcomes_ms) / len(outcomes_ms)
-  print(f"{task}: average time milliseconds: {average_time_ms:.2f}")
+  print(f"{task}: average time milliseconds of {tries} tries: {average_time_ms:.2f}")
   return average_time_ms
 
 
