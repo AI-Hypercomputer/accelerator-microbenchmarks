99fa6b1b1f872c30c79b664dcd5b2d9f276e972a
Hong-Yi Lin <horacehylin@google.com>
2026-01-27 09:49:40 +0000

    Update automation script and yaml files for different topologies.

--- Ironwood/guides/automation/automation_launch.sh
+++ Ironwood/guides/automation/automation_launch.sh
@@ -3,21 +3,20 @@
 ######################################################################
 #                            USER INPUT
 ######################################################################
-export GCS_BUCKET_ROOT_DIR=""
+export GCS_PATH=""
 
-yaml_names=("tpu7x-16-hbm.yaml")
-job_names=("tpu7x-16-hbm")
+yaml_names=("tpu7x-2x2x1-hbm.yaml" "tpu7x-2x2x2-hbm.yaml")
+job_names=("tpu7x-8-hbm" "tpu7x-16-hbm")
 
 ######################################################################
 #                        VALIDATION & SETUP
 ######################################################################
 
-if [[ -z "${GCS_BUCKET_ROOT_DIR}" || "${GCS_BUCKET_ROOT_DIR}" != "gs://"* ]]; then
-  echo "Error: GCS_BUCKET_ROOT_DIR must be set and start with gs://"
+if [[ -z "${GCS_PATH}" || "${GCS_PATH}" != "gs://"* ]]; then
+  echo "Error: GCS_PATH must be set and start with gs://"
   exit 1
 fi
-TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
-export GCS_PATH="${GCS_BUCKET_ROOT_DIR}/${TIMESTAMP}"
+
 echo "The intermediate result will be written to ${GCS_PATH}"
 
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
@@ -29,13 +28,26 @@ fi
 #                 LAUNCH JOBS & WAIT FOR COMPLETION
 ######################################################################
 
-for yaml_file in "${yaml_names[@]}"; do
+length=${#yaml_names[@]}
+for (( i=0; i<length; i++ )); do
+    yaml_file=${yaml_names[${i}]}
+    export JOB_NAME=${job_names[${i}]}
     echo "Launch job: ${yaml_file}"
-    envsubst '${GCS_PATH}' < ${yaml_file} | kubectl apply -f -
+    envsubst '${JOB_NAME} ${GCS_PATH}' < ${yaml_file} | kubectl apply -f -
+    (
+        echo "Job launched successfully!"
+        if kubectl wait --for=condition=complete job/${JOB_NAME} --timeout=1800s &> /dev/null; then
+            echo "Job from ${yaml_file} is completed!"
+        else
+            echo "Job from ${yaml_file} failed!"
+            
+        fi
+        envsubst '${JOB_NAME} ${GCS_PATH}' < ${yaml_file} | kubectl delete -f -
+    ) &
 done
 
-for job_name in "${job_names[@]}"; do
-    kubectl wait --for=condition=complete job/${job_name} --timeout=1800s
-    kubectl delete job ${job_name}
-done
+echo "All jobs dispatched. Waiting for results..."
+wait
+echo "All processing done."
+
 kubectl apply -f aggregator.yaml
