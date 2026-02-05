bf5c79cf574359c52227938bcef47bc4d2392386
Leonard Chan <leoch@google.com>
2026-02-05 06:07:41 +0000

    Add h2d_type column to H2D/D2H output

--- Ironwood/src/benchmark_host_device.py
+++ Ironwood/src/benchmark_host_device.py
@@ -184,6 +184,7 @@ def benchmark_host_device_calculate_metrics(
     }
     metadata = {k: v for k, v in params if k in metadata_keys}
     metadata["dtype"] = "float32"
+    metadata["h2d_type"] = h2d_type
     
     metrics = {}
     
