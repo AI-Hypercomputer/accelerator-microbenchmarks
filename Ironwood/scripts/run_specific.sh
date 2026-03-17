1bab3ab5ba3476d2f4c5c03818b1fc8fb887630c
Rahul Sharma <rahulasharma@google.com>
2026-03-17 16:55:39 +0000

    up

---
+++ Ironwood/scripts/run_specific.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Run command: sh ./Ironwood/scripts/run_ici_microbenchmark.sh
+
+
+topology=$1
+demo=$2
+filter=$3
+
+if [ -z "$topology" ]; then
+  echo "Error: Topology argument is missing."
+  echo "Usage: $0 <topology> [true|false]"
+  exit 1
+fi
+
+CONFIG_DIR="Ironwood/configs/collectives/$topology"
+
+if [ ! -d "$CONFIG_DIR" ]; then
+  echo "Error: Configuration directory '$CONFIG_DIR' does not exist."
+  exit 1
+fi
+
+for CONFIG_FILE in "$CONFIG_DIR"/*.yaml
+do
+  CONFIG_NAME=$(basename "$CONFIG_FILE" .yaml)
+  if [ -z "$filter" ] || [[ "$CONFIG_NAME" == *"$filter"* ]]; then
+    continue
+  fi
+  
+  echo "--- Starting benchmark for ${CONFIG_NAME} ---"
+  
+  # Run the python script and wait for it to complete
+  if [ "$demo" = "true" ]; then
+    python3 Ironwood/src/run_benchmark.py --config="${CONFIG_FILE}" --demo
+  else
+    python3 Ironwood/src/run_benchmark.py --config="${CONFIG_FILE}"
+  fi
+  
+  echo "--- Finished benchmark for ${CONFIG_NAME} ---"
+done
