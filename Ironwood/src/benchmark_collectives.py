16e15b946eb026f3933e0d8721b4a6d5447cfe7b
Rahul Sharma <rahulasharma@google.com>
2026-04-24 17:02:08 +0000

    up

--- Ironwood/src/benchmark_collectives.py
+++ Ironwood/src/benchmark_collectives.py
@@ -391,6 +391,7 @@ def psum_scatter_benchmark(
       "--xla_tpu_use_tc_device_shape_on_sc=true",
       "--xla_tpu_enable_3d_reduce_scatter_decomposer=false",
       f"--xla_tpu_dvfs_p_state={GLOBAL_PSTATE}",
+      "--xla_tpu_use_single_sparse_core_for_reduce_scatter_offload=false",
   ]
   # libtpu_init_args = [
   #     "--xla_jf_debug_level=3",
